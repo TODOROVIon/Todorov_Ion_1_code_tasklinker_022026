2d7b5f7151378591da3d2cced444628741b04900
Todorov Ion <ion@atidgo.ai>
2026-03-31 07:31:11 +0200

    Creation controlleur equipe + modif sur view

--- src/Controller/UsersController.php
+++ src/Controller/UsersController.php
@@ -2,17 +2,21 @@
 
 namespace App\Controller;
 
+use App\Repository\UsersRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 final class UsersController extends AbstractController
 {
-    #[Route('/users', name: 'app_users')]
-    public function index(): Response
+    #[Route('/list', name: 'app_equipe')]
+    public function index(UsersRepository $usersRepository): Response
     {
+        $users = $usersRepository->findAll();
+
         return $this->render('users/index.html.twig', [
             'controller_name' => 'UsersController',
+            'users' => $users,
         ]);
     }
 }
