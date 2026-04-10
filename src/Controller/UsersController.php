3dc45ce202aa363ed8b9f63f5779ea0a793f7936
Todorov Ion <ion@atidgo.ai>
2026-04-10 12:10:34 +0200

    edit utilisateur

--- src/Controller/UsersController.php
+++ src/Controller/UsersController.php
@@ -2,21 +2,47 @@
 
 namespace App\Controller;
 
+use App\Form\UserType;
+use App\Repository\StatusRepository;
 use App\Repository\UsersRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 final class UsersController extends AbstractController
 {
     #[Route('/list', name: 'app_equipe')]
-    public function index(UsersRepository $usersRepository): Response
+    public function index(UsersRepository $usersRepository, StatusRepository $statusRepository): Response
     {
         $users = $usersRepository->findAll();
+        $status = $statusRepository->findAll();
 
-        return $this->render('users/users.html.twig', [
+        return $this->render('users/list.html.twig', [
             'controller_name' => 'UsersController',
             'users' => $users,
+            'status' => $status,
+        ]);
+    }
+
+    #[Route('/show/{id}', name: 'app_users_show', requirements: ['id' => '\d+'])]
+    public function show(int $id, UsersRepository $usersRepository, Request $request, EntityManagerInterface $entityManager): Response
+    {
+        $user = $usersRepository->find($id);
+
+        $form = $this->createForm(UserType::class, $user);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->flush();
+
+            return $this->redirectToRoute('app_equipe');
+        }
+
+        return $this->render('users/user.html.twig', [
+            'user' => $user,
+            'form' => $form->createView(),
         ]);
     }
 }
---
+++ src/Form/UserType.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Users;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class UserType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('first__name')
+            ->add('last_name')
+            ->add('email')
+            ->add('contract_type')
+            ->add('enter_date', DateType::class, [
+                'label' => 'Date',
+                'widget' => 'single_text',
+                'html5' => true,
+                'required' => false,
+                'attr' => ['class' => 'form-control']
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Users::class,
+        ]);
+    }
+}
